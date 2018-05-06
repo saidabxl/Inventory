708ff38580412eba270a6e1ba8994e8465b50362
saidabxl <qunsulka@gmail.com>
2018-05-06 18:08:30 +0200

    Initial commit

---
+++ app/src/main/java/com/example/android/inventory/data/InventoryContract.java
@@ -0,0 +1,52 @@
+package com.example.android.inventory.data;
+
+import android.content.ContentResolver;
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+public class InventoryContract {
+
+    public static final String CONTENT_AUTHORITY =  "com.example.android.inventory";
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    public static final String PATH_INVENTORY = "product";
+
+    //constructor
+    public InventoryContract() {
+    }
+
+    public final static class InventoryEntry implements BaseColumns {
+
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_INVENTORY);
+
+        public static final String CONTENT_LIST_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" +  PATH_INVENTORY;
+
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_INVENTORY;
+
+        //Name of DB table
+        public final static String TABLE_NAME = "product";
+
+        //Column of table
+        public final static String _ID = BaseColumns._ID;
+        public final static String COLUMN_PRODUCT_NAME = "product_name";
+        public final static String COLUMN_PRODUCT_PRICE = "price";
+        public final static String COLUMN_PRODUCT_QUANTITY = "quantity";
+        public final static String COLUMN_PRODUCT_SUPPLIER_NAME = "supplier_name";
+        public final static String COLUMN_PRODUCT_SUPPLIER_PHONE_NUMBER = "supplier_phone_number";
+
+        // SUPPLIER_NAME LIST VALUES
+        public final static int SUPPLIER_UNKNOWN = 0;
+        public final static int SUPPLIER_AMAZON = 1;
+        public final static int SUPPLIER_EBAY= 2;
+        public final static int SUPPLIER_LULU = 3;
+
+        public static boolean isValidSupplierName(int suppliername) {
+            if (suppliername == SUPPLIER_UNKNOWN || suppliername == SUPPLIER_AMAZON || suppliername == SUPPLIER_EBAY|| suppliername == SUPPLIER_LULU) {
+                return true;
+            }
+            return false;
+        }
+    }
+}
